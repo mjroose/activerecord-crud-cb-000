6bce2ed428f02feb7242b7b794e63ff5c18cefde
Michael Roose <mjroose@gmail.com>
2018-09-30 15:46:25 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "movies", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.integer "release_date"
     t.string "director"
     t.string "lead"
